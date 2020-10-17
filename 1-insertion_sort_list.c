cc4e0b94c7bbf2e8cb74dc522c303f18308ed7c0
Andres802 <reachandres@gmail.com>
2020-10-17 22:56:32 +0000

    insertion algo

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,57 +9,46 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *fwd, *rwd;
-	int i, j, k;
-
-	i = 1;
+	listint_t *fwd, *rwd, *tmp;
+	int j, k;
 
 	fwd = *list;
 	while (fwd != NULL)
 	{
 		j = 1;
 		rwd = fwd;
-		printf("\n--- [ %.02d - fwd.n = %d ] ---\n", i, fwd->n);
-		printf("\nbefore: ");
-		print_list(*list);
-		if (rwd->prev != NULL)
-			printf("rwd.n = [ %d < %d ] = rwd.prev.n\n", rwd->n, rwd->prev->n);
-		printf("J = %d\n\n", j);
+		
 		while (rwd->prev != NULL && rwd->n < rwd->prev->n)
 		{
-			printf("--- [ %.02d | %.02d ] ---\n", i, j);
+			tmp = rwd->prev;
 			/* prev = rwd - 1; next = rwd + 1 */
-			printf("1 ");
 			if (rwd->prev->prev != NULL)
 				rwd->prev->prev->next = rwd;
-			printf("2 ");
+			
 			if (rwd->next != NULL)		     /* if next is not NULL */
 				rwd->next->prev = rwd->prev; /* next's previous is rwd's previous */
-			printf("3 ");
+			
 			if (rwd->prev != NULL)
 			{
-				rwd->prev->next = rwd->next;
-				printf("4 ");
+				rwd->prev->next = rwd->next;	
 				rwd->prev = rwd->prev->prev;
 			}
-			printf("5 ");
 			if (rwd->next != NULL)
 				rwd->next = rwd->next->prev;
-			printf("6 ");
+			
 			if (rwd->next != NULL)
 				rwd->next->prev = rwd;
-			printf("7\n");
+			if (rwd->next == NULL)
+			{
+				rwd->next = tmp;
+				tmp->prev = rwd;
+			}
 			if (rwd->prev == NULL)
 				*list = rwd;
-
-			printf("after: ");
-			print_list(*list);
-			if (rwd->prev != NULL)
-				printf("rwd.n = [ %d < %d ] = rwd.prev.n\n", rwd->n, rwd->prev->n);
 			j++;
-			printf("J = %d\n\n", j);
+			print_list(*list);
 		}
-		i++;
+
 		for (k = 0; k < j; k++)
 			if (fwd != NULL)
 				fwd = fwd->next;
