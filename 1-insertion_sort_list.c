2001ffa63d90ec42f928104282c2a36005f83edb
gorgyboy <jlrogb@gmail.com>
2020-10-17 16:42:58 -0500

    refactor code and fix linking

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,39 +10,58 @@
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *fwd, *rwd;
-	int i;
+	int i, j, k;
+
+	i = 1;
 
 	fwd = *list;
-
 	while (fwd != NULL)
 	{
+		j = 1;
 		rwd = fwd;
+		printf("\n--- [ %.02d - fwd.n = %d ] ---\n", i, fwd->n);
+		printf("\nbefore: ");
+		print_list(*list);
+		if (rwd->prev != NULL)
+			printf("rwd.n = [ %d < %d ] = rwd.prev.n\n", rwd->n, rwd->prev->n);
+		printf("J = %d\n\n", j);
 		while (rwd->prev != NULL && rwd->n < rwd->prev->n)
 		{
-			i = 0;
-			printf("%d#  ", i++);
-			print_list(*list);
+			printf("--- [ %.02d | %.02d ] ---\n", i, j);
 			/* prev = rwd - 1; next = rwd + 1 */
-			if (rwd->next != NULL) /* if next is not NULL */
-				rwd->next->prev = rwd->prev; /* next's previous
-							* is rwd's previous
-							*/
-			printf("%d#  ", i++);
+			printf("1 ");
+			if (rwd->prev->prev != NULL)
+				rwd->prev->prev->next = rwd;
+			printf("2 ");
+			if (rwd->next != NULL)		     /* if next is not NULL */
+				rwd->next->prev = rwd->prev; /* next's previous is rwd's previous */
+			printf("3 ");
+			if (rwd->prev != NULL)
+			{
+				rwd->prev->next = rwd->next;
+				printf("4 ");
+				rwd->prev = rwd->prev->prev;
+			}
+			printf("5 ");
+			if (rwd->next != NULL)
+				rwd->next = rwd->next->prev;
+			printf("6 ");
+			if (rwd->next != NULL)
+				rwd->next->prev = rwd;
+			printf("7\n");
+			if (rwd->prev == NULL)
+				*list = rwd;
+
+			printf("after: ");
 			print_list(*list);
-			rwd->prev->next = rwd->next;
-			printf("%d#  ", i++);
-			print_list(*list);
-			rwd->prev = rwd->prev->prev;
-			printf("%d#  ", i++);
-			print_list(*list);
-			rwd->next = rwd->next->prev;
-			printf("%d#  ", i++);
-			print_list(*list);
-			rwd->next->prev = rwd;
-			printf("%d#  ", i++);
-			print_list(*list);
-			rwd = rwd->prev;
+			if (rwd->prev != NULL)
+				printf("rwd.n = [ %d < %d ] = rwd.prev.n\n", rwd->n, rwd->prev->n);
+			j++;
+			printf("J = %d\n\n", j);
 		}
-		fwd = fwd->next;
+		i++;
+		for (k = 0; k < j; k++)
+			if (fwd != NULL)
+				fwd = fwd->next;
 	}
 }
