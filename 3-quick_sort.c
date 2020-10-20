3a49df124d5f21b1eb6284f1806bd7d78cd13b20
gorgyboy <jlrogb@gmail.com>
2020-10-19 20:30:55 -0500

    Fix check position

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -66,12 +66,15 @@ size_t partition(int *array, size_t lo, size_t hi, size_t size)
 			tmp = array[i_pivot];
 			array[i_pivot] = array[i_sort];
 			array[i_sort] = tmp;
+			if (i_sort != i_pivot)
+				print_array(array, size);
 			i_pivot++;
 		}
 	}
 	tmp = array[i_pivot];
 	array[i_pivot] = array[hi];
 	array[hi] = tmp;
-	print_array(array, size);
+	if (i_sort != i_pivot)
+		print_array(array, size);
 	return (i_pivot);
 }
